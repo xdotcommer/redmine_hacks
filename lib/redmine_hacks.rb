0197ca3627d91cab9b0b10855c7c6fb67dc0c895
Michael Cowden <mikecowden@razoo.com>
2011-03-17 15:49:36 -0400

    basic framework for redmine hacks

---
+++ lib/redmine_hacks.rb
@@ -0,0 +1,4 @@
+class RedmineHacks < Redmine::Hook::ViewListener
+  # render_on :view_issues_sidebar_planning_bottom, :inline => '<div id="sidebar_queries" style="display: none;">'
+  # render_on :view_issues_sidebar_queries_bottom, :partial => 'hooks/raz_hacks/foldable_queries'
+end
