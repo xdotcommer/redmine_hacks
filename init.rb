23f639c395d9cab4551d581e0cfc50088ff5a6ed
Michael Cowden <mikecowden@razoo.com>
2011-03-22 16:35:00 -0400

    require the issue model

--- init.rb
+++ init.rb
@@ -1,2 +1,10 @@
 require 'redmine'
 require_dependency 'redmine_hacks'
+require File.join(File.dirname(__FILE__), 'app', 'models', 'issue')
+
+Redmine::Plugin.register :redmine_hacks do
+  name 'Redmine Hacks for Razoo'
+  author 'Michael Cowden'
+  description 'This provides some Razoo-specific hacks to Redmine'
+  version '0.0.1'
+end
