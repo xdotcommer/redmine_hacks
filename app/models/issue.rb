a7dba3e118c237f89f7d6d3887fefbc3d20f2c8d
Michael Cowden <mikecowden@razoo.com>
2011-05-13 15:21:23 -0400

    seeing if hooks on the issue model work in redmine

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue
   after_save :set_position_to_backlog_rank
   
   def set_position_to_backlog_rank
+    logger.error(">>>>>>>>>>>>>>set_position_to_backlog_rank<<<<<<<<<<<<<<")
     if rank = custom_values.find(:first, :conditions => "custom_fields.name = 'Backlog Rank'", :include => :custom_field)
       self.position = rank.value
       save!
--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 require_dependency 'redmine_hacks'
-require 'app/models/issue'
+require File.join(File.dirname(__FILE__), 'app', 'models', 'issue')
 
-# Redmine::Plugin.register :redmine_hacks do
-#   name 'Redmine Hacks for Razoo'
-#   author 'Michael Cowden'
-#   description 'This provides some Razoo-specific hacks to Redmine'
-#   version '0.0.1'
-# end
+Redmine::Plugin.register :redmine_hacks do
+  name 'Redmine Hacks for Razoo'
+  author 'Michael Cowden'
+  description 'This provides some Razoo-specific hacks to Redmine'
+  version '0.0.1'
+end
